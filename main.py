6ab9db7e155c2ede482dee7ac13606e779d60789
Sami Rautalahti <sami.rautalahti@student.lab.fi>
2024-02-12 16:02:33 +0200

    Koodin päivitys

--- main.py
+++ main.py
@@ -3,30 +3,20 @@ import datetime as dt
 import pyodbc
 import numpy as np
 
-# Retrieve the latest hour of data from a bounding box
-end_time = dt.datetime.utcnow()
-start_time = end_time - dt.timedelta(hours=1)
-# Convert times to properly formatted strings
-start_time = start_time.isoformat(timespec="seconds") + "Z"
-# -> 2020-07-07T12:00:00Z
-end_time = end_time.isoformat(timespec="seconds") + "Z"
-# -> 2020-07-07T13:00:00Z
-
-START_TIME = dt.datetime(2023,1,1,0,0,0)
-END_TIME = dt.datetime(2023,12,31,0,0,0)
-ARGS =["place=Lappeenranta",
-       "starttime=" + START_TIME.isoformat(timespec="auto"),
-       "endtime=" + END_TIME.isoformat(timespec="auto")]
+START_TIME = dt.datetime(2023, 12, 1, 0, 0, 0)
+END_TIME = dt.datetime(2023, 12, 30, 0, 0, 0)
+ARGS = ["place=Lappeenranta",
+        "starttime=" + START_TIME.isoformat(timespec="auto"),
+        "endtime=" + END_TIME.isoformat(timespec="auto")]
 
 obs = download_stored_query("fmi::observations::weather::daily::multipointcoverage",
                             args=ARGS)
 
 # Uusi for-loop kaikkien havaintoasemien viimeisimpien tietojen tulostamiseksi
 for time, data in obs.data.items():
-        print(f"\nTime: {time}")
-        for variable, value in data.items():
-            print(f"{variable}: {value}")
-
+    print(f"\nTime: {time}")
+    for variable, value in data.items():
+        print(f"{variable}: {value}")
 
 # Yhdistä SQL Server -tietokantaan
 server = ' '
@@ -35,66 +25,63 @@ username = 'DESKTOP-368NHB7\\sampp'
 password = ''
 driver = '{ODBC Driver 17 for SQL Server}'
 
-connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection=yes;Encrypt=yes;TrustServerCertificate=yes'
+connection_string = (f'DRIVER={driver};SERVER={server};DATABASE={database};'
+                     f'Trusted_Connection=yes;Encrypt=yes;TrustServerCertificate=yes')
 connection = pyodbc.connect(connection_string, autocommit=True)
 cursor = connection.cursor()
 
 # Etsi viimeisin aikasäde
-latest_tstep = max(obs.data.keys())
+#latest_tstep = max(obs.data.keys())
 
 # Luo tietokantataulu, jos se ei ole vielä olemassa
 cursor.execute('''
-    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'säädata')
+    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'December2023')
     BEGIN
-        CREATE TABLE säädata (
-            asema NVARCHAR(255),
-            aika NVARCHAR(255),
-            tuulen_nopeus FLOAT,
-            ilmanlämpötila FLOAT,
-            pilvisyys INT,
-            ilmankosteus FLOAT,
-            lumensyvyys FLOAT
+        CREATE TABLE December2023 (
+            Station NVARCHAR(255),
+            Time NVARCHAR(255),
+            Max_temperature FLOAT,
+            Min_temperature FLOAT,
+            Avg_temperature FLOAT,
+            Precipitation FLOAT,
+            Snow_depth FLOAT
         )
     END
 ''')
 connection.commit()
 
-# Lisää tiedot tietokantaan
-wind_speed_values = []
 air_temperature_values = []
-cloud_amount_values = []
-relative_humidity_values = []
+max_temp_values = []
+min_temp_values = []
+precipitation_values = []
 snow_depth_values = []
 
+# Lisää tiedot tietokantaan
 for time, data in obs.data.items():
     for station, value in data.items():
-        wind_speed = float(value.get("Wind speed", {}).get("value", 0.0))
         air_temperature = float(value.get("Air temperature", {}).get("value", 0.0))
-        cloud_amount = int(value.get("Cloud amount", {}).get("value", 0))
-        relative_humidity = float(value.get("Relative humidity", {}).get("value", 0.0))
+        max_temp = float(value.get("Maximum temperature", {}).get("value", 0.0))
+        min_temp = float(value.get("Minimum temperature", {}).get("value", 0.0))
+        precipitation = float(value.get("Precipitation amount", {}).get("value", 0.0))
         snow_depth = float(value.get("Snow depth", {}).get("value", 0.0))
 
-        # Append values to lists
-        wind_speed_values.append(wind_speed)
-        air_temperature_values.append(air_temperature)
-        cloud_amount_values.append(cloud_amount)
-        relative_humidity_values.append(relative_humidity)
-        snow_depth_values.append(snow_depth)
-
-    # Käsittele 'nan'-arvot ennen lisäämistä tietokantaan
-    wind_speed = wind_speed if not np.isnan(wind_speed) else 0.0
-    air_temperature = air_temperature if not np.isnan(air_temperature) else 0.0
-    snow_depth = snow_depth if not np.isnan(snow_depth) else 0.0
+    air_temperature_values.append(air_temperature)
+    max_temp_values.append(max_temp)
+    min_temp_values.append(min_temp)
+    precipitation_values.append(precipitation)
+    snow_depth_values.append(snow_depth)
 
     try:
         print(
-            f"Values before inserting for station {station}: {wind_speed}, {air_temperature}, {cloud_amount}, {relative_humidity}, {snow_depth}")
+            f"Values before inserting for station {station}: {air_temperature}, {max_temp}, {min_temp},"
+            f" {precipitation}, {snow_depth}")
 
         cursor.execute('''
-            INSERT INTO säädata (asema, aika, tuulen_nopeus, ilmanlämpötila, pilvisyys, ilmankosteus, lumensyvyys)
+            INSERT INTO December2023 (Station, Time, Avg_temperature, Max_temperature, Min_temperature, Precipitation, 
+            Snow_depth)
             VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', (station, time.strftime('%Y-%m-%d %H:%M:%S'), wind_speed, air_temperature, cloud_amount,
-              relative_humidity, snow_depth))
+        ''', (station, time.strftime('%Y-%m-%d'), air_temperature, max_temp, min_temp,
+              precipitation, snow_depth))
 
     except Exception as e:
         print(f"Virhe lisättäessä tietoja asemalle {station}: {e}")
